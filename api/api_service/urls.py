f023d6e746baecee69d1954c5ee0630d8e6c4b14
pasha <pashakorsh@gmail.com>
2024-09-16 09:30:46 +0300

    swagger added

--- api/api_service/urls.py
+++ api/api_service/urls.py
@@ -3,6 +3,9 @@ from config.views import RateViewSet
 from config.views import UserRegistrationView
 from django.urls import include
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView
+from drf_spectacular.views import SpectacularRedocView
+from drf_spectacular.views import SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
@@ -18,5 +21,16 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path(
         "api/user/register/", UserRegistrationView.as_view(), name="user_registration"
+    ),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
     ),
 ]
--- api/config/views.py
+++ api/config/views.py
@@ -3,6 +3,7 @@ from config.models import Rate
 from config.serializers import ConfigSerializer
 from config.serializers import RateSerializer
 from config.serializers import UserRegistrationSerializer
+from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
@@ -23,6 +24,11 @@ class RateViewSet(viewsets.ModelViewSet):
 class UserRegistrationView(APIView):
     permission_classes = [AllowAny]
 
+    @extend_schema(
+        request=UserRegistrationSerializer,
+        responses={201: UserRegistrationSerializer, 400: "Error"},
+        description="Endpoint for user registration",
+    )
     def post(self, request, *args, **kwargs):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
